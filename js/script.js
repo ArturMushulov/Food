1d0a0785edba96d93fef278028d0dfd856dc14a1
Artur Mushulov <bigmanar2r@gmail.com>
2022-07-11 11:43:16 +0300

    food_page

---
+++ js/script.js
@@ -0,0 +1,36 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import calc  from './modules/calc';
+import cards from './modules/cards';
+import form from './modules/form';
+import modal from './modules/modal';
+import slide from './modules/slide';
+import tabs from './modules/tabs';
+import timer from './modules/timer'; 
+import {openModal} from './modules/modal';
+
+window.addEventListener("DOMContentLoaded", () => { 
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 3000);
+    
+    calc();
+    cards();
+    form('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    timer('.timer', '2022-08-03');
+    slide({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slide-inner'
+    });
+    
+});
+
+
+
